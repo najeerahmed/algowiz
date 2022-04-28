4251292a0ae1d82157d7072efa61ae0d042cca72
swittuth <swittuthcoding@gmail.com>
2022-04-28 17:50:10 -0400

    edit logo

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
 <!DOCTYPE html>
 <head>
     <title>Algo Wiz</title>
+    <link rel="stylesheet" href="css/styles.css"></link>
 </head>
 <body>
-    
+    <img id="logo" src="assets/algo-wiz-logo.png"/>
+    <form method="POST" action="sign_in.php">
+        <input type="username" name="username" placeholder="Enter Username"/>
+        <input type="password" name="password" placeholder="Enter Password"/>
+        <button>Login</button>
+    </form>
 </body>
