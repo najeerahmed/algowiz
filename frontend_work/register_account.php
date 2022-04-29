5a8b93a763d399e0f5f32cb4be4a48e76b22e892
swittuth <swittuthcoding@gmail.com>
2022-04-29 19:35:29 -0400

    edit style to the register account page / added requirement

--- frontend_work/register_account.php
+++ frontend_work/register_account.php
@@ -7,9 +7,32 @@
     <head>
         <title>Register Account</title>
         <link rel="stylesheet" href="../css/styles.css"/>
+        <link rel="stylesheet" href="../css/register_acc_styles.css"/>
     </head>
     <body>
         <img id="logo" src="../assets/algo-wiz-logo.png"/>
+
+        <main id="register-account-area">
+            <form>
+                <input type="username" name="username" placeholder="Register Username" required/>
+                <input type="password" name="password" placeholder="Register Password" required/>
+                <button type="submit">Register Account</button>
+            </form>
+
+            <aside id="requirement-satisfaction">
+                <ul>
+                    <li>
+                        Have a special character (!, @, #, etc.)
+                    </li>
+                    <li>
+                        Include a number 
+                    </li>
+                    <li>
+                        Length must be longer then 7 characters
+                    </li>
+                </ul>
+            </aside>
+        </main>
 
     </body>
 
