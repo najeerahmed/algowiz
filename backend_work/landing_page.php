bd4e611227178fd0810aa2ccda326817a3bea670
Najeer Ahmed <najeerkdahmed@gmail.com>
2022-04-30 21:07:27 -0400

    Added landing page, return questions

---
+++ backend_work/landing_page.php
@@ -0,0 +1,86 @@
+<?php
+    include_once "../backend_work/connect_server.php";
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Welcome!</title>
+        <link rel="stylesheet" href="../css/styles.css"/>
+        <link rel="stylesheet" href="../css/register_acc_styles.css"/>
+    </head>
+    <body>
+        <img id="logo" src="../assets/algo-wiz-logo.png"/>
+    
+        <main id="question-area" method=$_POST>
+            <form>
+                <fieldset>
+                <legend>Recently Posted Questions</legend>
+                <?php
+                    $sql_recent_questions = "
+                    select title, q_time
+                    from Questions
+                    order by q_time desc
+                    limit 10";
+                
+                    #$result = $conn->query($sql_recent_questions)
+                    if ($stmt = $conn->prepare("SELECT title, question_id from Questions order by q_time desc limit 10")) {
+                        $stmt->execute();
+                        $stmt->bind_result($title,$question_id);
+                        echo "<table border = '1'>
+                        <tr>
+                        </tr>";
+                    
+                        while($stmt->fetch())
+                        {
+                            echo"<tr>";
+                            echo "<td><a href='return_question_page.php?question_id_num=$question_id'>$title</a></td>";
+                            echo"</tr>";
+                        }
+                        echo "</table>";
+                        $stmt->close();
+                    }
+                ?>
+                </fieldset>
+            </form>
+
+            <form>
+                <fieldset>
+                <legend>Recently Answered Questions</legend>
+                <?php
+                    $sql_recent_questions = "
+                    select a_title, a_time
+                    from Answers
+                    order by a  _time desc
+                    limit 10";
+                
+                    #$result = $conn->query($sql_recent_questions)
+                    if ($stmt = $conn->prepare("SELECT title, Users.username, status_title
+                    from Answers join Users on (Answers.user_id = Users.user_id) join Questions on (Answers.question_id = Questions.question_id) join UserStatus on (Users.user_id = UserStatus.user_id) join StatusDict on (UserStatus.status_id = StatusDict.status_id)
+                    order by a_time desc
+                    limit 10")) {
+                        $stmt->execute();
+                        $stmt->bind_result($title,$username,$status_title);
+                        echo "<table border = '1'>
+                        <tr>
+                        </tr>";
+                    
+                        while($stmt->fetch())
+                        {
+                            echo"<tr>";
+                            echo "<td><a href='return_question_page.php?question_id_num=$question_id'>$title</a></td>";
+                            echo "<td>$username</td>";
+                            echo "<td>$status_title</td>";
+                            echo"</tr>";
+                        }
+                        echo "</table>";
+                        $stmt->close();
+                    }
+                ?>
+                </fieldset>
+            </form>
+        </main>
+    </body>
+
+</html>
+
---
+++ backend_work/return_question_page.php
@@ -0,0 +1,73 @@
+<?php
+    include_once "../backend_work/connect_server.php";
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Welcome!</title>
+        <link rel="stylesheet" href="../css/styles.css"/>
+        <link rel="stylesheet" href="../css/register_acc_styles.css"/>
+    </head>
+    <body>
+        <img id="logo" src="../assets/algo-wiz-logo.png"/>
+    
+        <main id="question-area" method=$_POST>
+            <form>
+                <fieldset>
+                <legend>Recently Posted Questions</legend>
+                <?php
+                    $question_id = $_GET['question_id_num'];
+                    $sql_recent_questions = "
+                    select title, q_time
+                    from Questions
+                    order by q_time desc
+                    limit 10";
+                
+                    #$result = $conn->query($sql_recent_questions)
+                    if ($stmt = $conn->prepare("SELECT title from Questions where Questions.question_id=?")) {
+                        $stmt->bind_param("i", $question_id);
+                        $stmt->execute();
+                        $stmt->bind_result($title);
+                        echo "<table border = '1'>
+                        <tr>
+                        </tr>";
+                    
+                        while($stmt->fetch())
+                        {
+                            echo"<tr>";
+                            echo "<td>$title</td>";
+                            echo"</tr>";
+                        }
+                        echo "</table>";
+                        $stmt->close();
+                    }
+
+                    if ($stmt = $conn->prepare("SELECT a_text, username from Questions join Answers on (Questions.question_id = Answers.question_id) join Users on (Answers.user_id = Users.user_id) where Questions.question_id=?")) {
+                        $stmt->bind_param("i", $question_id);
+                        $stmt->execute();
+                        $stmt->bind_result($a_text, $username);
+                        echo "<table border = '1'>
+                        <tr>
+                        </tr>";
+                    
+                        while($stmt->fetch())
+                        {
+                            echo"<tr>";
+                            echo "<td>$username</td>";
+                            echo "<td>$a_text</td>";
+                            echo"</tr>";
+                        }
+                        echo "</table>";
+                        $stmt->close();
+                    }
+
+                ?>
+                </fieldset>
+            </form>
+
+        </main>
+    </body>
+
+</html>
+
