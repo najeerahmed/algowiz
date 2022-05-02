a7e29e779ad9a90c17b3534d9803d4a83f0f052a
swittuth <swittuthcoding@gmail.com>
2022-05-01 23:07:10 -0400

    pulled information into the user profile page to prepare for layout

--- frontend_work/user_profile.php
+++ frontend_work/user_profile.php
@@ -9,17 +9,25 @@
             $username = $_GET["username"];
 
             $sql = "
-                select uname, points
-                from UsersLogin natural join UsersInfo
-                where username = '$username';
-            ";
+                    select uname, status_title, points, email, city, state, country, dob, short_desc
+                    from UsersLogin natural join UsersInfo
+                    join UserStatus on UsersLogin.user_id = UserStatus.user_id
+                    join StatusDict on UserStatus.status_id = StatusDict.status_id
+                    where UsersLogin.username = '$username';
+                ";
 
-            $result = mysqli_query($conn, $sql);
-            // because login authenticated then user information 
-            // is definitely there
-            // no need to perform check anymore
-            $row = mysqli_fetch_assoc($result);
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result); // should only be one row
+
             $name = $row["uname"];
+            $rank = $row["status_title"];
+            $points = $row["points"];
+            $email = $row["email"];
+            $city = $row["city"];
+            $state = $row["state"];
+            $country = $row["country"];
+            $dob = $row["dob"];
+            $short_desc = $row["short_desc"];
 
             echo "<title>$name's Profile Page</title>";
         ?>
@@ -31,14 +39,21 @@
     <body>
         <main>
             <?php
-                echo "<h1>$name</h1>";
-                $sql = "
-                    select status_title
-                    from UsersLogin natural join UsersInfo
-                    join UserStatus on UsersLogin.user_id = UserStatus.user_id
-                    join StatusDict on UserStatus.status_id = StatusDict.status_id
-                    where UsersLogin.username = '$username';
+                echo "
+                <h1>$name</h1>
+
+                <section id='status_section'>
+                    <h3>$rank, </h3>
+                    <h3>$points</h3>
+                </section>
+
+                <p>
+                    $short_desc
+                </p>
+
+
                 ";
+
             ?>
         </main>
     </body>
