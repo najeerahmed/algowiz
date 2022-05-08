771972ef56e73f4e400684c1bd9812dceba75e91
swittuth <swittuthcoding@gmail.com>
2022-05-08 00:06:49 -0400

    removed form from landing page leaving only table

--- frontend_work/landing_page.php
+++ frontend_work/landing_page.php
@@ -6,9 +6,9 @@
 <html>
     <head>
         <title>Algo-Wiz</title>
+        <link rel="stylesheet" href="../css/landing_styles.css"/>
         <link rel="stylesheet" href="../css/styles.css"/>
         <link rel="stylesheet" href="../css/nav_styles.css"/>
-        <link rel="stylesheet" href="../css/landing_styles.css"/>
     
     </head>
     <body>
@@ -37,72 +37,62 @@
                 </button>
             </a>
             
-            <form>
-                <fieldset>
-                <legend>Recently Posted Questions</legend>
-                <?php
-                    $sql_recent_questions = "
-                    select title, q_time
-                    from Questions
-                    order by q_time desc
-                    limit 10;";
-                
-                    #$result = $conn->query($sql_recent_questions)
-                    if ($stmt = $conn->prepare("SELECT title, question_id, username from Questions join UsersLogin on (Questions.user_id = UsersLogin.user_id) order by q_time desc limit 10")) {
-                        $stmt->execute();
-                        $stmt->bind_result($title,$question_id,$username);
-                        echo "<table border = '1'>
-                        <tr>
-                        </tr>";
-                    
-                        while($stmt->fetch())
-                        {
-                            echo"<tr>";
-                            echo "<td><a href='return_question_page.php?question_id_num=$question_id'>$title</a></td>";
-                            echo "<td><a href='user_profile.php?username=$username'>$username</a></td>";
-                            echo"</tr>";
-                        }
-                        echo "</table>";
-                        $stmt->close();
-                    }
-                ?>
-                </fieldset>
-            </form>
-
-            <form>
-                <fieldset>
-                <legend>Recently Answered Questions</legend>
-                <?php
-                    $sql_recent_questions = "
-                    select a_title, a_time
-                    from Answers
-                    order by a  _time desc
-                    limit 10";
+            <!-- should reimplement with proper link rather than display with form -->
+            <?php
+                $sql_recent_questions = "
+                select title, q_time
+                from Questions
+                order by q_time desc
+                limit 10;";
             
-                    if ($stmt = $conn->prepare("SELECT title, UsersLogin.username, status_title, Answers.question_id
-                    from Answers join UsersLogin on (Answers.user_id = UsersLogin.user_id) join Questions on (Answers.question_id = Questions.question_id) join UserStatus on (UsersLogin.user_id = UserStatus.user_id) join StatusDict on (UserStatus.status_id = StatusDict.status_id)
-                    order by a_time desc
-                    limit 10")) {
-                        $stmt->execute();
-                        $stmt->bind_result($title,$username,$status_title,$question_id);
-                        echo "<table border = '1'>
-                        <tr>
-                        </tr>";
-                    
-                        while($stmt->fetch())
-                        {
-                            echo"<tr>";
-                            echo "<td><a href='return_question_page.php?question_id_num=$question_id'>$title</a></td>";
-                            echo "<td><a href='user_profile.php?username=$username'>$username</a></td>";
-                            echo "<td>$status_title</td>";
-                            echo"</tr>";
-                        }
-                        echo "</table>";
-                        $stmt->close();
+                if ($stmt = $conn->prepare("SELECT title, question_id, username from Questions join UsersLogin on (Questions.user_id = UsersLogin.user_id) order by q_time desc limit 10")) {
+                    $stmt->execute();
+                    $stmt->bind_result($title,$question_id,$username);
+                    echo "<table border = '1'>
+                    <tr>
+                    </tr>";
+                
+                    while($stmt->fetch())
+                    {
+                        echo"<tr>";
+                        echo "<td><a href='return_question_page.php?question_id_num=$question_id'>$title</a></td>";
+                        echo "<td><a href='user_profile.php?username=$username'>$username</a></td>";
+                        echo"</tr>";
                     }
-                ?>
-                </fieldset>
-            </form>
+                    echo "</table>";
+                    $stmt->close();
+                }
+            ?>
+
+            <?php
+                $sql_recent_questions = "
+                select a_title, a_time
+                from Answers
+                order by a  _time desc
+                limit 10";
+        
+                if ($stmt = $conn->prepare("SELECT title, UsersLogin.username, status_title, Answers.question_id
+                from Answers join UsersLogin on (Answers.user_id = UsersLogin.user_id) join Questions on (Answers.question_id = Questions.question_id) join UserStatus on (UsersLogin.user_id = UserStatus.user_id) join StatusDict on (UserStatus.status_id = StatusDict.status_id)
+                order by a_time desc
+                limit 10")) {
+                    $stmt->execute();
+                    $stmt->bind_result($title,$username,$status_title,$question_id);
+                    echo "<table border = '1'>
+                    <tr>
+                    </tr>";
+                
+                    while($stmt->fetch())
+                    {
+                        echo"<tr>";
+                        echo "<td><a href='return_question_page.php?question_id_num=$question_id'>$title</a></td>";
+                        echo "<td><a href='user_profile.php?username=$username'>$username</a></td>";
+                        echo "<td>$status_title</td>";
+                        echo"</tr>";
+                    }
+                    echo "</table>";
+                    $stmt->close();
+                }
+            ?>
         </main>
     </body>
 
